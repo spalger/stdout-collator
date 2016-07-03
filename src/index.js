8dfc625832414290e44c39e5ec9617ec79234869
spalger <email@spalger.com>
2016-07-02 23:39:26 -0700

    expose bindToStdout() method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import { Parser } from './Parser'
+
 export { Parser } from './Parser'
+export { Group } from './Group'
 export { GroupMapper } from './GroupMapper'
 export * from './marks'
+
+export function bindToStdout() {
+  const parser = new Parser()
+  process.stdout.writeWithoutParsing = process.stdout.write
+  process.stdout.write = (chunk, encoding, callback) => {
+    parser.write(chunk, encoding)
+    callback()
+  }
+  process.on('beforeExit', () => {
+    parser.close()
+  })
+  return parser
+}
